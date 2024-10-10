ae5bcf7ad1eb2f254b9f3dc22fa09c9f99c0267c
Léo José da Costa <leoeduar@gmail.com>
2024-10-10 07:07:58 -0300

    Add test

--- src/test/java/com/assis/coleta/ColetaApplicationTests.java
+++ src/test/java/com/assis/coleta/ColetaApplicationTests.java
@@ -1,13 +1,29 @@
 package com.assis.coleta;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
 
 @SpringBootTest
 class ColetaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @org.springframework.beans.factory.annotation.Autowired
+    private AgendamentoService agendamentoService;
+
+    com.github.javafaker.Faker faker = new com.github.javafaker.Faker();
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void saveAgendamentoTest() {
+        com.assis.coleta.Agendamento agendamento = new com.assis.coleta.Agendamento();
+        agendamento.setCliente(faker.name().name());
+        agendamento.setCidade(faker.country().capital());
+        agendamento.setMaterial(faker.animal().name());
+        agendamento.setQuantidade(faker.number().numberBetween(1, 100));
+        agendamento.setData(java.time.LocalDate.from(faker.date().birthday(4, 1).toInstant()));
+        agendamentoService.add(agendamento);
+    }
 
 }
