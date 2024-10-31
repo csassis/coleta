ac24d12bc6d0343ca026df32de6cff9be42a627e
Assis Cristiano <cristiano.1000@hotmail.com>
2024-10-31 18:54:40 -0300

    remove files

--- src/main/java/com/assis/coleta/AgendamentoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.assis.coleta;
-
-@org.springframework.stereotype.Repository
-public interface AgendamentoRepository extends org.springframework.data.repository.CrudRepository<Agendamento, Integer>{
-
-}
